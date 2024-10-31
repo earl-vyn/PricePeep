1e9f58c87dcb4cdca7d917ff8c98dc4aa5f1af86
unknown <2100802548@std.kyu.ac.ug>
2024-10-31 16:17:14 +0300

    a

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -21,17 +21,6 @@ export async function scrapeAmazonProduct(url: string) {
     host: 'brd.superproxy.io',
     port,
     rejectUnauthorized: false,
-    headers: {
-      // Add more realistic browser-like headers
-      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-      'Accept-Language': 'en-US,en;q=0.9',
-      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-      'Sec-Fetch-Dest': 'document',
-      'Sec-Fetch-Mode': 'navigate',
-      'Sec-Fetch-Site': 'none',
-      'Sec-Fetch-User': '?1',
-      'Upgrade-Insecure-Requests': '1', },
-    timeout: 1000,
   }
 
   try {
@@ -91,15 +80,6 @@ export async function scrapeAmazonProduct(url: string) {
 
     return data;
   } catch (error: any) {
-    console.log("Scraping error", error.message);
-
-    if (error.response) {
-      console.error('Response status:', error.response.status);
-      console.error('Response data:', error.response.data);
-    }
-
-    throw new Error('Failed to scrape Amazon product');
+    console.log(error);
   }
-
-  
 }
