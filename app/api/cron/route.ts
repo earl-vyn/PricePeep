cd9c5604d8f08e555135b0681ddc109d4a681221
unknown <2100802548@std.kyu.ac.ug>
2024-10-31 14:30:22 +0300

    cron job output minimised

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -73,8 +73,8 @@ export async function GET(request: Request) {
     );
 
     return NextResponse.json({
-      message: "Ok",
-      data: updatedProducts,
+      message: "Product details updated successfully",
+      data: updatedProducts.length,
     });
   } catch (error: any) {
     throw new Error(`Failed to get all products: ${error.message}`);
